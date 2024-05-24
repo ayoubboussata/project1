59f3ae45673c78fc4ea6cc0cc35136f8056670d8
ayoubboussata <145572726+ayoubboussata@users.noreply.github.com>
2024-05-24 16:12:06 +0200

    push

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const userRoutes = require('./src/routes/userRoutes');
 const postRoutes = require('./src/routes/postRoutes');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+   res.send('Hello World!');
+});
 
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`);
