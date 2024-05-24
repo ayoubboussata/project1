71331c571e252587e30920ac5f1e86c894af5ade
ayoubboussata <145572726+ayoubboussata@users.noreply.github.com>
2024-05-24 16:38:53 +0200

    push

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./src/routes/userRoutes');
 const postRoutes = require('./src/routes/postRoutes');
 
-const app = express();
+const app = express(); // Move this line to the top
 const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
